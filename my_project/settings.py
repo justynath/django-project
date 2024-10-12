c207c244f63a54dded2c566245550f559b5350e1
Justyna Thompson <justynabp@gmail.com>
2024-10-12 16:13:01 +0000

    Update code for deployment: install gunicorn, create Procfile, update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-islo_l1dp+j%z!k7!4g$5!^7li(gy(8b@6m40^i36_krft=vbf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-justynath-djangoproject-5soi0d4bzud.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-justynath-djangoproject-5soi0d4bzud.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
